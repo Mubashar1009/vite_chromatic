e2f0c3ccc3aa51d8b2cb348b022dbb60d2d0dc85
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-05-21 17:59:07 +0500

    fix:added virewoports

---
+++ .storybook/mode.ts
@@ -0,0 +1,20 @@
+export const allModes = {
+  mobile_small: {
+    name: "Mobile M",
+    styles: { width: "375px", height: "667px" },
+  },
+  mobile_medium: {
+    name: "Mobile L",
+    styles: { width: "425px", height: "736px" },
+  },
+  large: { name: "Tablet", styles: { width: "768px", height: "1024px" } },
+  laptop_small: {
+    name: "Laptop M",
+    styles: { width: "1024px", height: "768px" },
+  },
+  default: { name: "Default", styles: { width: "1280px", height: "720px" } },
+  laptop_large: {
+    name: "Laptop L",
+    styles: { width: "1440px", height: "900px" },
+  },
+};
