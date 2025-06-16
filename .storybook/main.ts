f36f7079e90e7d8edd76c75608098e0acdbca8d3
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-06-16 14:04:59 +0500

    updated main.ts file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,32 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest",
+  addons: [
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
     '@storybook/addon-themes',
-    '@storybook-addon-performance'
+    '@storybook-addon-performance',
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      build: {
+        ...config.build,
+        sourcemap: 'hidden',
+      },
+    };
+  },
 };
+
 export default config;
