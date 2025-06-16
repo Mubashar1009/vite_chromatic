761e2f6f1907f8f7f62f2a60383c4d56a0af5416
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-06-16 13:01:51 +0500

    chore:Added bundlesize

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import bundlesize from "vite-plugin-bundlesize";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(), bundlesize(),],
+  build: {
+        sourcemap: "hidden",
+     },
 })
