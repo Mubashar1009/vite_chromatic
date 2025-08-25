05e9896bab65d2bb64a1a68e0d540ece06ee9ca3
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-08-25 13:38:51 +0500

    worked on lighouse cli

---
+++ lighthouserc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: process.env.LHCI_URL??'http://localhost:5173/',
+        startServerCommand: process.env.LHCI_URL??undefined,
+      },
+      assert: {
+       
+        assertions: {
+          "categories:performance": ["warn", {"minScore": 0.9}],
+          "categories:accessibility": ["error", {"minScore": 1}]
+        }
+      },
+      upload: {
+        target: 'temporary-public-storage',
+      },
+    },
+  };
