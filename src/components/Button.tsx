76e7c32dfbe8586caba2c35d88141abd509866d1
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-08-25 14:53:56 +0500

    fix: Fixed build issue

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 export const Button = ({label}:{label:string}) => {
   return (
     <button className='px-4 py-2 rounded bg-white text-black dark:bg-black dark:text-white'>
