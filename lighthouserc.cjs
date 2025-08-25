4fe088d7c21c5b15cbac5e2b0f7359fdeef34178
Mubashar Shakeel <moonmubashar945@gmail.com>
2025-08-25 15:36:46 +0500

    fix: Fixed lighthouse issue

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,18 +1,33 @@
-module.exports = {
-    ci: {
-      collect: {
-        url: process.env.LHCI_URL??'http://localhost:5173/',
-        startServerCommand: process.env.URL??undefined,
-      },
-      assert: {
-       
-        assertions: {
-          "categories:performance": ["warn", {"minScore": 0.9}],
-          "categories:accessibility": ["error", {"minScore": 1}]
-        }
-      },
-      upload: {
-        target: 'temporary-public-storage',
+/* eslint-disable no-undef */
+
+module.exports =  {
+  ci: {
+    collect: {
+      url: [process.env.LHCI_URL || "http://localhost:3000"], 
+      startServerCommand: process.env.LHCI_START || undefined,
+      numberOfRuns: 2,
+      settings: {
+        onlyCategories: [
+          'performance',
+          'accessibility',
+          'best-practices',
+          'seo',
+        ]
+      ,
+    chromeFlags: '--no-sandbox',
+    }
+
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.95 }],
+        "categories:seo": ["error", { minScore: 0.9 }],
+        "categories:best-practices": ["error", { minScore: 0.9 }],
       },
     },
-  };
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
